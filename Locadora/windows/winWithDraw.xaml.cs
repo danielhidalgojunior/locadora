60b35539d1d71b058f902a0cb747701446420acd
Daniel Hidalgo <danielhidalgo@cooperative.com.br>
2018-06-12 13:38:25 -0300

    Created window for withdraws

--- Locadora/windows/winWithDraw.xaml.cs
+++ Locadora/windows/winWithDraw.xaml.cs
@@ -2,6 +2,7 @@
 using Locadora.models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,89 @@ namespace Locadora.windows
     /// </summary>
     public partial class winWithDraw : Window
     {
-        public winWithDraw(MovieItem movie, EmployeeModel employee)
+        ObservableCollection<WithDrawModel> Withdraws { get; set; }
+        MovieItem Movie { get; set; }
+        EmployeeModel Employee { get; set; }
+
+        public winWithDraw(MovieItem movie = null, EmployeeModel employee = null)
         {
             InitializeComponent();
+
+            if (movie == null)
+            {
+                (tab.Items[0] as TabItem).IsEnabled = false;
+                tab.SelectedIndex = 1;
+                return;
+            }
+
+            Movie = movie;
+            Employee = employee;
+
+            txt_id.Text = movie.MovieModel.Id.ToString();
+            txt_movietitle.Text = movie.MovieModel.Title;
+            img_image.Source = movie.Img;
+            txt_units.Text = movie.MovieModel.Units.ToString();
+
+            dt_returndate.SelectedDate = DateTime.Today.AddDays(5);
+        }
+
+        private void UpdateTable()
+        {
+            Withdraws = new ObservableCollection<WithDrawModel>(MovieStoreManager.GetAllWithDraws());
+            table.ItemsSource = Withdraws;
+        }
+
+        private void ClearTxts()
+        {
+            cb_clients.SelectedIndex = -1;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            if (cb_clients.SelectedIndex == -1)
+                return;
+
+            WithDrawModel withdraw = new WithDrawModel()
+            {
+                MovieTitle = Movie.MovieModel.Title,
+                ClientId = (cb_clients.SelectedItem as ClientModel).Id,
+                Employee = Employee.Id,
+                WithDrawReturn = (DateTime)dt_returndate.SelectedDate,
+                WithDrawDate = DateTime.Today
+            };
+
+            if (Movie.MovieModel.Units < 1)
+            {
+                MessageBox.Show("Não existem unidades disponíveis para este filme!", "Falta de unidades", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (WithDrawModel.Save(withdraw))
+            {
+                Movie.MovieModel.Units--;
+
+                MovieModel.Replace(Movie.MovieModel);
+                MovieStoreManager.Movies = MovieStoreManager.GetAllMovies();
+
+                MessageBox.Show("Retirada bem sucessida!");
+                txt_units.Text = Movie.MovieModel.Units.ToString();
+
+                ClearTxts();
+                UpdateTable();
+            }
+            else
+            {
+                MessageBox.Show("Erro ao tentar retirar filme! Tente novamente.");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var clients = MovieStoreManager.GetAllClients();
+
+            cb_clients.ItemsSource = clients;
+
+            UpdateTable();
         }
     }
 }
